0d4e08b4f83baac38989171a0d1f02a722816457
Sang Ho Lee <33576729+lsho76@users.noreply.github.com>
2020-10-29 21:34:35 -0400

    Update main.R

--- main.R
+++ main.R
@@ -1,1 +1,50 @@
+library(doSNOW)
+library(ggplot2)
+library(snow)
+library(githubinstall)
+library(caret)
+library(pROC)
+library(devtools)
+library(plotROC)
+library(xlsx)
+library(tibble)
+library(mixOmics)
 
+Y<-as.factor(pretx_class)
+preCT<-cbind(pre_shape, preCT_radiomics_texture$firstorder_original, preCT_radiomics_texture$secondorder_original, preCT_radiomics_texture$higherorder_original, preCT_radiomics_texture$global_original)
+prePET<-cbind(prePET_radiomics_texture$firstorder_original, prePET_radiomics_texture$secondorder_original, prePET_radiomics_texture$higherorder_original, prePET_radiomics_texture$global_original)
+CT <- preCT
+PET <- prePET
+binary_class <- Y
+
+Y <- as.factor(binary_class)
+X <- list(CT = CT, PET = PET)
+list.keepX <- list(CT = c(seq(1, 4, 1)), PET = c(seq(1, 4, 1)))
+
+s = 3
+w = 1
+design = matrix(1, ncol = s, nrow = s)
+diag(design) =  0
+design[1,s]=w
+design[s,1]=w
+
+library(parallelMap)
+parallelStartMulticore(cpus=10, mc.set.seed=TRUE)
+
+set.seed(1, "L'Ecuyer")
+tune.block.splsda <- mixOmics::tune.block.splsda(X, Y, ncomp = 4, validation = 'Mfold', folds = 5, design=design, near.zero.var = TRUE, light.output = FALSE, dist="max.dist",
+                                                 test.keepX = list.keepX, nrepeat = 20, cpus=10)
+parallelStop()
+
+tune.block.splsda$choice.keepX
+tune.block.splsda$choice.ncomp$ncomp
+
+choice.ncomp <- tune.block.splsda$choice.ncomp$ncomp
+choice.keepX <- list(CT=tune.block.splsda$choice.keepX$CT[1:choice.ncomp], PET=tune.block.splsda$choice.keepX$PET[1:choice.ncomp])
+
+MyResult.diablo <- block.splsda(X, Y, ncomp = choice.ncomp, keepX=choice.keepX)
+selectedVariables<-selectVar(MyResult.diablo, comp = choice.ncomp)
+
+set.seed(1, "L'Ecuyer")
+perf.block.splsda <- perf(MyResult.diablo, validation = "Mfold", folds = 5, progressBar = TRUE, auc = TRUE, nrepeat = 20, dist="max.dist") 
+perf.block.splsda$auc
